166989eed84deaed39ce8d0012a6376cd6a79be0
Sebastian Johnsson <sebastian.johnsson@gmail.com>
2020-01-08 08:15:23 +0700

    Add delete action for configure resource

--- resources/configure.rb
+++ resources/configure.rb
@@ -1,12 +1,15 @@
 default_action :create
 
-property :domain,       String, name_attribute: true
-property :binary,       String, default: lazy { "/opt/certbot/certbot-auto" }
-property :config_path,  String
-property :rsa_key_size, Integer, default: 4096
-property :email,        String
-property :hostnames,    String
-property :webroot_path, String
+property :domain,             String, name_attribute: true
+
+property :binary,             String, default: '/opt/certbot/certbot-auto'
+property :installation_dir,   String, default: '/etc/letsencrypt'
+
+property :config_path,        String
+property :rsa_key_size,       Integer, default: 4096
+property :email,              String
+property :hostnames,          String
+property :webroot_path,       String
 
 action :create do
   template new_resource.template_path do
@@ -35,6 +38,22 @@ action :create do
     
     not_if do
       new_resource.webroot_path.to_s.empty?
+    end
+  end
+end
+
+action :delete do
+  files                   =   [
+    "#{new_resource.installation_dir}/configs/#{new_resource.domain}.conf",
+    "#{new_resource.installation_dir}/renewal/#{new_resource.domain}.conf",
+    "#{new_resource.installation_dir}/live/#{new_resource.domain}",
+    "#{new_resource.installation_dir}/archive/#{new_resource.domain}"
+  ]
+  
+  files.each do |remove_file|
+    file remove_file do
+      action :delete
+      only_if { ::File.exists?(remove_file) }
     end
   end
 end
